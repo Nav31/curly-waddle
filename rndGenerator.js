eec46e5e3f771fb7188adfd926e6ad7bf5184052
Nav31 <nsingh3104@gmail.com>
2016-06-02 11:49:50 -0400

    refactored code a bit

--- rndGenerator.js
+++ rndGenerator.js
@@ -4,27 +4,22 @@
 // It selects 4 unique random numbers range 1 - 75
 // Then select 1 from range 1 - 15
 
+var rando = (num) => Math.floor(Math.random() * (num)+1)
+
+// This is to pick 1 number 
+var pick1 = () => rando(15)
+
+// This is to pick 4 numbers
 var pick4 = () => {
 	var result = [];
-	var holder = 0;
-	for(var i = 0; i < 4; i++) {
-		result.push(Math.floor(Math.random() * (75) + 1))
-	}
+	for(var i = 0; i < 4; i++) {result.push(rando(75))}
 	return result;
 }
 
-
-var pick1 = () => {
-	var result = 0;
-	result = Math.floor(Math.random() * (15) + 1)
-	return result;
-}
 
 // This function only for testing on rnd numbers
 
-for(var i = 0; i< 1000; i++) {
-	console.log(pick4())
-}
+for(var i = 0; i< 1000; i++) {console.log(pick4())}
 
 // TODO: Make sure the numbers in pick4 function are unique.
 // As in you can't have [1,2,3,1] ie 1 is appearing twice.  
