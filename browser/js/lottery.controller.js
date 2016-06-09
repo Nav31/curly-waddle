6bab7f2a5895911a701b394ce548d68742a383f9
Nav31 <nsingh3104@gmail.com>
2016-06-09 16:18:33 -0400

    jackpots show on screen

--- browser/js/lottery.controller.js
+++ browser/js/lottery.controller.js
@@ -3,20 +3,23 @@
 app.controller('lotteryCtrl', ($scope, LotteryFactory, $log) => {
 	let getInfo = info => $scope.info = info.odds;
 	let getNums = numbers => $scope.nums = numbers;
-	let getJackpots = jackpots => $scope.value = jackpots
+	let megaVal = jackpots => $scope.value = jackpots[0].concat(" ", jackpots[1]);
+	let powerVal = jackpots => $scope.value = jackpots[2].concat(" ",jackpots[3])
+
 	$scope.hidden = true;
 
 	$scope.getMegaMillions = () => {
 		$scope.hidden = false;
 		LotteryFactory.getMegaMillions().then(getNums)
 		$scope.info = LotteryFactory.getMegaMillionsInfo().then(getInfo)
-		$scope.value = LotteryFactory.getJackpots().then(getJackpots) //$scope.value = jackpots[0]
+		$scope.value = LotteryFactory.getJackpots().then(megaVal) 
 		.catch($log);
 	}
 	$scope.getPowerball = () => {
 		$scope.hidden = false;
 		LotteryFactory.getPowerBall().then(getNums)
 		$scope.info = LotteryFactory.getPowerBallInfo().then(getInfo)
+		$scope.value = LotteryFactory.getJackpots().then(powerVal)
 		.catch($log);
 	}
 	$scope.getPlay3 = () => {
